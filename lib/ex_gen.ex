0475efa8eb5f303ba3795a479bf5ccdf74359cc6
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2018-02-20 08:51:33 +0100

    Use Mix.raise instead on raise

--- lib/ex_gen.ex
+++ lib/ex_gen.ex
@@ -164,32 +164,32 @@ defmodule ExGen do
     file = opts[:config] || System.user_home!() |> Path.join(".xgen.exs")
 
     unless File.regular?(file) do
-      raise """
+      Mix.raise("""
       #{file} does not exist.
 
       You must provide a configuration file. Either create a global one using
       `mix xgen.config.create` or provide one passing the `--config <file>`
       option to the generator.
-      """
+      """)
     end
 
     {config, _} = Code.eval_file(file)
 
     unless Keyword.keyword?(config) do
-      raise "The config file must contain a keyword list."
+      Mix.raise("The config file must contain a keyword list.")
     end
 
     unless config[:name] |> String.valid?() do
-      raise """
+      Mix.raise("""
       The configuration file must contain a `:name` key and it must be a string.
-      """
+      """)
     end
 
     unless config[:github_account] |> String.valid?() do
-      raise """
+      Mix.raise("""
       The configuration file must contain a `:github_account` key and it must be
       a string.
-      """
+      """)
     end
 
     config
