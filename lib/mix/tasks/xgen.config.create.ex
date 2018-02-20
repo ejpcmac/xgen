47985208c56179b520a25eb7923da5142d14b3b2
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2018-02-20 22:59:36 +0100

    Add a mix task to generate a configuration file

---
+++ lib/mix/tasks/xgen.config.create.ex
@@ -0,0 +1,35 @@
+defmodule Mix.Tasks.Xgen.Config.Create do
+  use Mix.Task
+
+  @shortdoc "Generates a configuration file for ExGen"
+
+  @moduledoc """
+  Generates a configuration file for ExGen.
+  """
+
+  @impl true
+  def run(_args) do
+    config = [
+      name: ask_name(),
+      github_account: ask_github_account()
+    ]
+
+    System.user_home!()
+    |> Path.join(".xgen.exs")
+    |> File.write!(Macro.to_string(quote(do: unquote(config))))
+  end
+
+  defp ask_name do
+    case "Full name:" |> Mix.shell().prompt() |> String.trim() do
+      "" -> ask_name()
+      name -> name
+    end
+  end
+
+  defp ask_github_account do
+    case "GitHub account:" |> Mix.shell().prompt() |> String.trim() do
+      "" -> ask_github_account()
+      account -> account
+    end
+  end
+end
