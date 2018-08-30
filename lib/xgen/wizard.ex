22d295942798774bfd1e0e7ac43fdd1da784c23b
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2018-08-30 22:57:31 +0200

    [Wizard] Introduce a choose/2 function

--- lib/xgen/wizard.ex
+++ lib/xgen/wizard.ex
@@ -62,6 +62,44 @@ defmodule XGen.Wizard do
   end
 
   @doc """
+  Asks the user to choose between a list of elements.
+
+  The given `list` must be a keyword list. The values will be printed and the
+  key of the chosen one returned.
+  """
+  @spec choose(String.t(), keyword()) :: atom()
+  def choose(message, list) do
+    info(message <> "\n")
+
+    list
+    |> Keyword.values()
+    |> Enum.with_index(1)
+    |> Enum.each(fn {elem, i} -> IO.puts("  #{i}. #{elem}") end)
+
+    index = list |> length() |> get_choice()
+
+    list
+    |> Enum.at(index - 1)
+    |> elem(0)
+  end
+
+  @spec get_choice(pos_integer()) :: pos_integer()
+  defp get_choice(max) do
+    with entry when entry != "" <- prompt("\nChoice:"),
+         {choice, _} when choice in 1..max <- Integer.parse(entry) do
+      choice
+    else
+      "" ->
+        error("You must make a choice!")
+        get_choice(max)
+
+      _ ->
+        error("The choice must be an integer between 1 and #{max}.")
+        get_choice(max)
+    end
+  end
+
+  @doc """
   Returns the user’s home or exits with an error.
   """
   @spec user_home :: String.t() | no_return()
