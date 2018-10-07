ac4bb0a488cd35a4798cf9d108ae457be3fee4b1
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2018-10-07 23:04:45 +0200

    Let the fun begin

--- mix.exs
+++ mix.exs
@@ -7,7 +7,17 @@ defmodule XGen.MixProject do
       version: "0.3.0-dev",
       elixir: "~> 1.6",
       escript: [main_module: XGen],
-      deps: deps()
+      deps: deps(),
+
+      # Tools
+      dialyzer: dialyzer(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: cli_env(),
+
+      # Docs
+      docs: [
+        main: "XGen"
+      ]
     ]
   end
 
@@ -20,6 +30,10 @@ defmodule XGen.MixProject do
       # Development dependencies
       {:credo, "~> 0.10.0", only: :dev, runtime: false},
       {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
+      {:excoveralls, ">= 0.0.0", only: :test, runtime: false},
+      {:mix_test_watch, ">= 0.0.0", only: :dev, runtime: false},
+      {:ex_unit_notifier, ">= 0.0.0", only: :test, runtime: false},
+      {:stream_data, "~> 0.4.0", only: :test},
 
       # Project dependencies
       {:ex_cli, "~> 0.1.4"},
@@ -27,6 +41,28 @@ defmodule XGen.MixProject do
 
       # Documentation dependencies
       {:ex_doc, "~> 0.19", only: :dev, runtime: false}
+    ]
+  end
+
+  # Dialyzer configuration
+  defp dialyzer do
+    [
+      plt_add_deps: :transitive,
+      flags: [
+        :unmatched_returns,
+        :error_handling,
+        :race_conditions
+      ],
+      ignore_warnings: ".dialyzer_ignore"
+    ]
+  end
+
+  defp cli_env do
+    [
+      # Always run coveralls mix tasks in `:test` env.
+      coveralls: :test,
+      "coveralls.detail": :test,
+      "coveralls.html": :test
     ]
   end
 end
--- shell.nix
+++ shell.nix
@@ -3,11 +3,21 @@
 with pkgs;
 
 let
+  inherit (lib) optional optionals;
+
   elixir = beam.packages.erlangR21.elixir_1_7;
   erlang = erlangR21;
   gitflow = gitAndTools.gitflow;
 in
 
 mkShell {
-  buildInputs = [ elixir erlang git gitflow ];
+  buildInputs = [ elixir erlang git gitflow ]
+    ++ optional stdenv.isLinux libnotify # For ExUnit Notifier on Linux.
+    ++ optional stdenv.isLinux inotify-tools # For file_system on Linux.
+    ++ optional stdenv.isDarwin terminal-notifier # For ExUnit Notifier on macOS.
+    ++ optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
+      # For file_system on macOS.
+      CoreFoundation
+      CoreServices
+    ]);
 }
