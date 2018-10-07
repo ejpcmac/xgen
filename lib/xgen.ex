b9287ac0333d2a109976c57c0d741d969bf74599
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2018-10-07 13:20:54 +0200

    Remove unneeded code

--- lib/xgen.ex
+++ lib/xgen.ex
@@ -2,19 +2,4 @@ defmodule XGen do
   @moduledoc """
   Helpers for Elixir projects generation.
   """
-
-  import XGen.Wizard
-
-  @spec fetch_config_file!(keyword()) :: map() | no_return()
-  def fetch_config_file!(opts) do
-    file = opts[:config] || System.user_home!() |> Path.join(".xgen.exs")
-
-    {config, _} = Code.eval_file(file)
-
-    unless is_map(config) do
-      halt("The config file must contain a map.")
-    end
-
-    config
-  end
 end
