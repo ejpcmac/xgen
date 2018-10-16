b03edfad2e5a9c00bbabcd7262b533bb691cd172
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2018-10-16 10:13:04 +0200

    [T/Elixir/*] Remove the dev version warning
    It can become very verbose when managing dependencies using it. The
    warning in the comment seems sufficient.

--- templates/_elixir_/_fragments_/mix.exs/dev.exs
+++ templates/_elixir_/_fragments_/mix.exs/dev.exs
@@ -1,11 +1,6 @@
 # Helper to add a development revision to the version. Do NOT make a call to
 # Git this way in a production release!!
 def dev do
-  if Mix.env() == :prod do
-    stacktrace = [{__MODULE__, :project, 0, [file: 'mix.exs']}]
-    IO.warn("You are using a development version.", stacktrace)
-  end
-
   with {rev, 0} <-
          System.cmd("git", ["rev-parse", "--short", "HEAD"],
            stderr_to_stdout: true
