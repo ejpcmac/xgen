14da433f3058c087bbbe2d583d5616d50b7a24d4
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2018-11-07 22:15:27 +0100

    [T/Elixir/*] Fix config template

--- config/config.exs
+++ config/config.exs
@@ -13,4 +13,6 @@ end
 
 # # Import environment specific config. This must remain at the bottom of this
 # # file so it overrides the configuration defined above.
-# import_config "#{Mix.env()}.exs"
+# unless Mix.env() == :docs do
+#   import_config "#{Mix.env()}.exs"
+# end
