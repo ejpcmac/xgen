5e730bfa66966e7e957c8e378bb6ace703158f87
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2018-11-07 22:20:04 +0100

    Use the :test env for mix test.watch

--- mix.exs
+++ mix.exs
@@ -31,7 +31,7 @@ defmodule XGen.MixProject do
       {:credo, "~> 0.10.0", only: :dev, runtime: false},
       {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
       {:excoveralls, ">= 0.0.0", only: :test, runtime: false},
-      {:mix_test_watch, ">= 0.0.0", only: :dev, runtime: false},
+      {:mix_test_watch, ">= 0.0.0", only: :test, runtime: false},
       {:ex_unit_notifier, ">= 0.0.0", only: :test, runtime: false},
       {:stream_data, "~> 0.4.0", only: :test},
 
@@ -60,6 +60,9 @@ defmodule XGen.MixProject do
 
   defp cli_env do
     [
+      # Run mix test.watch in `:test` env.
+      "test.watch": :test,
+
       # Always run Coveralls Mix tasks in `:test` env.
       coveralls: :test,
       "coveralls.detail": :test,
--- templates/_elixir_/_fragments_/mix.exs/deps/dev.exs
+++ templates/_elixir_/_fragments_/mix.exs/deps/dev.exs
@@ -2,6 +2,6 @@
 {:credo, "~> 0.10.0", only: :dev, runtime: false},
 {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
 {:excoveralls, ">= 0.0.0", only: :test, runtime: false},
-{:mix_test_watch, ">= 0.0.0", only: :dev, runtime: false},
+{:mix_test_watch, ">= 0.0.0", only: :test, runtime: false},
 {:ex_unit_notifier, ">= 0.0.0", only: :test, runtime: false},
 {:stream_data, "~> 0.4.0", only: :test}
