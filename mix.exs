ac76779d760dfd7ebd4a5f41695da074ddeaa6ef
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2018-11-16 08:14:39 +0100

    Bump the version to 0.3.5-dev

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule XGen.MixProject do
   def project do
     [
       app: :xgen,
-      version: "0.3.4",
+      version: "0.3.5" <> dev(),
       elixir: "~> 1.7",
       escript: [main_module: XGen],
       deps: deps(),
